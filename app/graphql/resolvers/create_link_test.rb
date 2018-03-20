93e28c7a1c9fb86e9526ca383140730a8a4ebd37
Charles Duporge <cduporge@ekylibre.com>
2018-03-20 18:13:07 +0100

    Test works

--- app/graphql/resolvers/create_link_test.rb
+++ app/graphql/resolvers/create_link_test.rb
@@ -1,15 +1,18 @@
 require 'test_helper'
 
-class Resolvers::CreateLink < ActiveSupport::TestCase
+class Resolvers::CreateLinkTest < ActiveSupport::TestCase
   def perform(args = {})
     Resolvers::CreateLink.new.call(nil, args, {})
   end
 
   test 'creating new link' do
-    link = perform({description: "Description", url: "http://ekylibre.com"})
+    link = perform(
+      url: 'http://ekylibre.com',
+      description: 'Description',
+     )
 
     assert link.persisted?
-    assert_equal link.description, "Description"
-    assert_equal link.url , "http://ekylibre.com"
+    assert_equal link.description, 'Description'
+    assert_equal link.url , 'http://ekylibre.com'
   end
 end
